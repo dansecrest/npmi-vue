1425fef2ae6c9df853bd7edaa5b7964caa71626b
Dan Secrest <dan.secrest@gmail.com>
2026-05-10 16:22:28 -0400

    Create Header and Footer

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,13 @@
+<template>
+  <footer class="site-footer">
+    <div class="copyright">
+      &copy;2026 North Point Ministries
+    </div>
+    <span>|</span>
+    <div class="links">
+      <a href="http://northpointministries.org/privacy" target="_blank">Privacy Policy</a>
+      <span>|</span>
+      <a href="http://northpointministries.org/terms" target="_blank">Terms of Use</a>
+    </div>
+  </footer>
+</template>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+defineProps<{
+  userName: string
+  church: {
+    name: string
+    fileName: string
+  }
+}>()
+</script>
+
+<template>
+  <header class="header">
+    <div class="site-header">
+      <RouterLink to="/" class="logo">
+        <img :src="`/images/church-icons/${church.fileName}.svg`" :alt="`${church.name}`" class="church-icon">
+        <img :src="`/images/church-logos/${church.fileName}.svg`" :alt="`${church.name}`" class="church-logo">
+      </RouterLink>
+
+      <div class="greeting">
+        <p>Welcome, <strong>{{ userName }}</strong></p>
+        <i class="fa fa-caret-down"></i>
+        <div class="menu">
+          <RouterLink to="/my-applications">My Applications</RouterLink>
+          <RouterLink to="/profile">My Profile</RouterLink>
+          <a href="#">Logout</a>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
