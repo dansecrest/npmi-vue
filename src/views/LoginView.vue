91e1ea9185fd7871a1649150dd1ba6c5e512d811
Dan Secrest <dan.secrest@gmail.com>
2026-05-10 17:55:50 -0400

    Add login view

---
+++ src/views/LoginView.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>Login To Your Account</h1>
+
+  <form>
+    <div class="form-group">
+      <label class="control-label">Email</label>
+      <input type="text" class="form-control">
+    </div>
+
+    <div class="form-group">
+      <label class="control-label">Password</label>
+      <input type="text" class="form-control">
+    </div>
+
+    <div class="actions">
+      <a href="#" class="btn btn-primary">Log In</a>
+      <a href="#" class="btn btn-action">Register</a>
+    </div>
+  </form>
+</template>
