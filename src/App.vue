65c32ada4c8395bec7550fb5161afd38e006bfe8
Dan Secrest <dan.secrest@gmail.com>
2026-05-10 17:56:08 -0400

    Add default header

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,19 @@
 <script setup lang="ts">
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
 import AppHeader from '@/components/AppHeader.vue'
-import AppFooter from './components/AppFooter.vue';
+import DefaultHeader from '@/components/DefaultHeader.vue'
+import AppFooter from '@/components/AppFooter.vue'
+
+const route = useRoute()
+const isAuthenticated = computed(() => route.meta.layout === 'app')
 </script>
 
 <template>
-  <AppHeader userName="Marge" :church="{ name: 'North Point Ministries', fileName: 'npm' }" />
-  <RouterView />
+  <AppHeader v-if="isAuthenticated" userName="Marge" :church="{ name: 'North Point Ministries', fileName: 'npm' }" />
+  <DefaultHeader v-else />
+  <main :class="isAuthenticated ? '' : 'login'">
+    <RouterView />
+  </main>
   <AppFooter />
 </template>
---
+++ src/components/DefaultHeader.vue
@@ -0,0 +1,16 @@
+<template>
+  <header class="header">
+    <div class="login-header">
+      <div class="logos">
+        <img src="/images/church-icons/white/browns-bridge.svg" alt="Browns Bridge Church">
+        <img src="/images/church-icons/white/buckhead.svg" alt="Buckhead Church">
+        <img src="/images/church-icons/white/decatur-city.svg" alt="Decatur City Church">
+        <img src="/images/church-icons/white/east-cobb.svg" alt="East Cobb Church">
+        <img src="/images/church-icons/white/gwinnett.svg" alt="Gwinnett Church">
+        <img src="/images/church-icons/white/hamilton-mill.svg" alt="Hamilton Mill Church">
+        <img src="/images/church-icons/white/north-point.svg" alt="North Point Community Church">
+        <img src="/images/church-icons/white/woodstock-city.svg" alt="Woodstock City Church">
+      </div>
+    </div>
+  </header>
+</template>
